5c0b587c6ca63c8ced10aa3e5a706af751d72df2
Marko Turchyn <marko.turchyn@gmail.com>
2020-09-09 16:12:00 -0400

    Changes added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        <Route exact path="/" component={About}  />
+        <Route path="/projects" component={Projects}  />
+        <Route path="/contact" component={Contact}  />
+
+        <header>
+        <div className="navigation">
+          
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Marko Turchyn</a>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <Link to="/" class="nav-link"> About me </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/projects" class="nav-link"> Projects </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/contact" class="nav-link"> Contacts </Link>
+            </li>
+          </ul>
+          </nav>
+          
+
+        </div>
+        </header>
+      </div>
+
+    </BrowserRouter>
   );
 }
 
